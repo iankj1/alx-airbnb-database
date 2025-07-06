2df7f65554674fe15b93ae92c83c3a1024678fc3
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-07-06 21:29:30 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -26,3 +26,53 @@ FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+
+
+-- Analyze the initial query's performance
+EXPLAIN SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
+
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+
+
+-- Refactored query to reduce execution time by limiting selected fields and ensuring indexed joins
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+
+    u.first_name,
+    u.last_name,
+
+    p.name AS property_name,
+
+    pay.amount
+
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
